121390fc696dd913f4c6eb67a40789289c2371d2
Caleb Madrigal <caleb.madrigal@gmail.com>
2014-08-28 15:23:32 -0400

    Added code.

---
+++ AsyncAwaitExample/AsyncAwaitExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main start");
+            Task<string> t = GetData();
+            Console.WriteLine("Main after call");
+            t.Wait();
+            string result = t.Result;
+            Console.WriteLine("Main end; result: " + result);
+            Console.ReadLine();
+        }
+
+        static async Task<string> GetData()
+        {
+            Console.WriteLine("\tBefore long operation");
+
+            await Task.Run(() =>
+            {
+                Console.WriteLine("\t\tTask before");
+                Thread.Sleep(3000);
+                Console.WriteLine("\t\tTask after");
+            });
+
+            Console.WriteLine("\tAfter long operation");
+
+            return "Hello world";
+        }
+    }
+}
